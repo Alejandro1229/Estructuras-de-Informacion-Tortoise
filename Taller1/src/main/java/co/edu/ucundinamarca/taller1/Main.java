64dabedef1ac73f218383b09c29c8ab6a4d71df2
Alejandro1229 <jalejandromanrique@ucundinamarca.edu.co>
2021-09-24 13:22:26 -0500

    * Añadir Taller1

---
+++ Taller1/src/main/java/co/edu/ucundinamarca/taller1/Main.java
@@ -0,0 +1,19 @@
+
+package co.edu.ucundinamarca.taller1;
+
+import org.apache.log4j.Logger;
+
+/**
+
+ */
+public class Main {
+
+    static Logger log = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        
+        log.info("Hello");
+ 
+    }
+
+}
